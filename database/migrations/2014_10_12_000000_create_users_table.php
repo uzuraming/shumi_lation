a6e1836a47b0dd14ecb0c1dc6bdcc358131899a3
uzuraming <b175975@example.com>
2020-12-06 14:56:43 +0900

    create user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile'); // プロフィール
+            $table->string('interest'); // 興味のあること
+            $table->string('wanted'); // 求めている人
             $table->rememberToken();
             $table->timestamps();
         });
