8f28bf90f02a097782cf655fdf25441d85a16dd7
uzuraming <b175975@gmail.com>
2021-01-03 17:06:01 +0900

    [add reactionテーブルを作成]

---
+++ database/migrations/2021_01_03_064706_create_reaction_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reaction', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('from_user_id');
+            $table->unsignedBigInteger('to_user_id'); 
+            $table->string('message'); // コンタクトメッセージ
+            $table->boolean('approval'); // 承認されているか否か
+
+            $table->timestamps();
+
+
+            // 外部キー制約
+            $table->foreign('from_user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('to_user_id')->references('id')->on('users')->onDelete('cascade');
+
+            // user_idとfollow_idの組み合わせの重複を許さない
+            $table->unique(['from_user_id', 'to_user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reaction');
+    }
+}
